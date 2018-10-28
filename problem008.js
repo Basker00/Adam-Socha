196157c4caeb710be6ae0fa847dd4d95b39957e2
Adam Socha <44506588+Basker00@users.noreply.github.com>
2018-10-28 16:50:56 +0100

    Update problem008.js

--- problem008.js
+++ problem008.js
@@ -5,12 +5,5 @@ function magicSequence(a){
     return -2;
   }else if(a==3){
     return -2;
-  }else return magicSequence(a - 1) * (a - 3);
-  
+  }else return magicSequence(a - 1) * (a - 3); 
 }
-
-var n=0;
-
-n = parseInt(prompt("Podaj nr wyrazu ciągu, którego wartość chcesz policzyć: "));
-
-console.log("Wartość w podanym elemencie ciągu wynosi: " + magicSequence(n));
