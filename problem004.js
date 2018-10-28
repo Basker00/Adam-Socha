7d2b33f272b2a6602d15ad57ecee8d4aabbe3b9b
Adam Socha <44506588+Basker00@users.noreply.github.com>
2018-10-28 21:01:22 +0100

    Update problem004.js

--- problem004.js
+++ problem004.js
@@ -3,7 +3,7 @@ function getTotalNumberOfNewYaersEveSundays(a, b){
   for(var i=a-1; i<=b-1; i++){
     var sunday=0;
     sunday = 1+26+8+i+(Math.floor(i/4))-(Math.floor(i/100))+(Math.floor(i/400))+2;
-    if(dzien % 7 == 1) x++;
+    if(sunday % 7 == 1) x++;
   }
   return x;
 }
