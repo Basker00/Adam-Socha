f057ff93ed16d1b3802fdd649793b8b4ec2ad2f4
Adam Socha <44506588+Basker00@users.noreply.github.com>
2018-11-12 17:58:46 +0100

    Update problem010.js

--- problem010.js
+++ problem010.js
@@ -1,5 +1,7 @@
 function deepCopy(a){
-  var ctab = new Array();
-  ctab = a.slice();
-  return ctab;
+  var newArray = [];
+
+  for (var i = 0; i < a.length; i++)
+      newArray[i] = a[i];
+  return console.log(newArray);
 }
