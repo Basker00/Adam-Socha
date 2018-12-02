cacc273dd8e6ef5ab9ad9ed21693eb9457426b10
Adam Socha <44506588+Basker00@users.noreply.github.com>
2018-12-02 16:40:25 +0100

    Update problem017.js

--- problem017.js
+++ problem017.js
@@ -1,9 +1,18 @@
 function compose(){
-  var tab = Array.prototype.slice.call(arguments);
+  var tab1 = Array.prototype.slice.call(arguments);
+  var tab = [];
+  for (var i=0; i<arguments.length; i++) {
+    tab[i]=tab1[i];
+  }
   return function(x) {
-    while(tab.length > 0) {
-      x = tab.pop()(x)
+    var i = tab.length-1;
+    while(i >= 0) {
+      x = tab[i](x);
+      i--;
+    }
+    for (var i=0; i<tab1.lenght; i++) {
+      arguments[i]=tab1[i];
     }
     return x;
-  };
+  }
 }
